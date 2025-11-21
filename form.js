ad737e298a2cefa2b961ba8cee17e0c8b5688468
liam <111153390+mailymailliam@users.noreply.github.com>
2025-11-21 10:11:29 +0100

    Update form.js

--- form.js
+++ form.js
@@ -1,19 +1,19 @@
 document.addEventListener("DOMContentLoaded",function () {
-    const form = document.querySelector("form");
+    const form=document.querySelector("form");
 
     form.addEventListener("submit",function (e) {
         e.preventDefault();
-
         if (!form.checkValidity()) {
             form.reportValidity();
-
             Swal.fire({
                 icon:"error",
                 title:"Oops...",
                 text:"Polje/-a ni/niso prav izpolnjeno/-na"
+				
             });
             return;
         }
+		else{
         Swal.fire({
             position:"center",
             icon:"success",
@@ -21,9 +21,8 @@ document.addEventListener("DOMContentLoaded",function () {
             showConfirmButton:false,
             timer:1500
         });
+		}
+
         setTimeout(()=>form.reset(),1500);
     });
 });
-
-
-
